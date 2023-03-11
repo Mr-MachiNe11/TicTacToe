fb3590236c02a05e3bb008ee3ac0ee6a039c85a8
machina <zrakib11@gmail.com>
2023-03-11 23:55:20 +0600

    Added button functionality

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1, btn2, btn3, btn4, btn5, btn6,btn7, btn8, btn9;
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,4 +33,20 @@ public class MainActivity extends AppCompatActivity {
         btn9 = findViewById(R.id.btn9);
 
     }
+
+    public void Check(View view){
+        Button btnClick = (Button) view;
+
+        if(flag==0){
+            btnClick.setText("X");
+            flag = 1;
+        }
+        else{
+            btnClick.setText("O");
+            flag = 0;
+        }
+
+    }
+
+
 }
