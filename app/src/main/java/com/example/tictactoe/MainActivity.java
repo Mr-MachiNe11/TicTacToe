9c8dbba822689af7ea859ac1a33639cea7c29742
machina <zrakib11@gmail.com>
2023-03-12 17:59:55 +0600

    added some conditions

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -62,21 +62,21 @@ public class MainActivity extends AppCompatActivity {
             //conditions
 
             if(b1.equals(b2) && b2.equals(b3) && !b1.equals("")){
-                
+               //1
             } else if (b4.equals(b5) && b5.equals(b6) && !b5.equals("")) {
-                
+                //2
             } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
-                
+                //3
             } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
-
+                //4
             } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
-
+                //5
             } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
-
+                //6
             } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
-
+                //7
             } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
-
+                //8
             }
 
         }
